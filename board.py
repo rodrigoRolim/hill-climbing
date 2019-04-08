918ce74cdf36ab2832ddb97a81324530d78e2516
Paulo Nakaima <nakaima.prk@gmail.com>
2019-04-08 02:42:04 -0300

    Add  bracket operator, find_queen and test

--- board.py
+++ board.py
@@ -1,10 +1,21 @@
 import numpy as np
 
 class Board:
+  def __init__(self):
+    self.board = np.zeros((8,8), dtype=int)
 
-	def __init__(self):
-		self.board = np.zeros((8,8), dtype=int)
-
-	def addQueen(self, row, column):
-		
-		self.board[row, column] = -1
+  def get(self, row, column):
+    return self.board[row, column]
+  
+  def __getitem__(self, key):
+    return self.board[key]
+  
+  def __setitem__(self, key, value):
+    self.board[key] = value
+    return self.board
+  
+  def find_queen(self, column):
+    return np.where(self.board[:, column] == -1)[0][0], column
+    
+  def print(self):
+    print(self.board)
--- test/board_test.py
+++ test/board_test.py
@@ -1,15 +1,51 @@
 import unittest
+import sys
+sys.path.append('../')
+from board import Board
 
 class TestBoard(unittest.TestCase):
-	def test_case01(self):
-		my_str = "ASHWIN "
-		my_int = 999
-		self.assertTrue(isinstance(my_str, str))
-		self.assertTrue(isinstance(my_int, int))
+  def test_init(self):
+    board = Board()
+    for i in range(8):
+      for j in range(8):
+        self.assertTrue(board[i, j] == 0)
+    board.print()
 
-	def test_case02(self):
-		my_pi = 3.14
-		self.assertFalse(isinstance(my_pi, int))
+  def test_add_queen(self):
+    board = Board()
+    i, j = 4, 0
+    board[i, j] = -1
+    self.assertTrue(board[i, j] == -1)
+    i, j = 5, 1
+    board[i, j] = -1
+    self.assertTrue(board[i, j] == -1)
+    i, j = 6, 2
+    board[i, j] = -1
+    self.assertTrue(board[i, j] == -1)
+    i, j = 3, 3
+    board[i, j] = -1
+    self.assertTrue(board[i, j] == -1)
+    i, j = 4, 4
+    board[i, j] = -1
+    self.assertTrue(board[i, j] == -1)
+    i, j = 5, 5
+    board[i, j] = -1
+    self.assertTrue(board[i, j] == -1)
+    i, j = 6, 6
+    board[i, j] = -1
+    self.assertTrue(board[i, j] == -1)
+    i, j = 5, 7
+    board[i, j] = -1
+    self.assertTrue(board[i, j] == -1)
+    board.print()
+  
+  def test_find_queen(self):
+    board = Board()
+    i, j = 4, 0
+    board[i, j] = -1
+    k, l = board.find_queen(j)
+    self.assertEquals(k, i)
+    self.assertEquals(l, j)
 
 if __name__ == '__main__':
 	unittest.main()
